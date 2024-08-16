ede1696ab2eac4bee2d8476effd062691c0bfc4b
Jimmy419 <suifeng419@126.com>
2024-08-16 17:50:53 +0800

    test

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,11 @@
 import NextAuth from "next-auth";
 import GitHub from "next-auth/providers/github";
-import CredentialsProvider from "next-auth/providers/credentials";
+import Credentials from "next-auth/providers/credentials";
 // import { connectToDb } from "./utils";
 import bcrypt from "bcryptjs";
-import { authConfig } from "./auth.config";
+// import { authConfig } from "./auth.config";
 import User from "@/server/model/User";
 
-console.log("bcrypt.compare*****************", bcrypt.compare);
 const login = async (credentials: any) => {
   try {
     // connectToDb();
@@ -37,13 +36,17 @@ export const {
   signIn,
   signOut,
 } = NextAuth({
-  ...authConfig,
+  // ...authConfig,
   providers: [
-    GitHub({
-      clientId: process.env.GITHUB_ID!,
-      clientSecret: process.env.GITHUB_SECRET!,
-    }),
-    CredentialsProvider({
+    // GitHub({
+    //   clientId: process.env.GITHUB_ID!,
+    //   clientSecret: process.env.GITHUB_SECRET!,
+    // }),
+    Credentials({
+      credentials: {
+        username: {},
+        password: {},
+      },
       // @ts-ignore
       async authorize(credentials) {
         try {
@@ -55,7 +58,64 @@ export const {
       },
     }),
   ],
+  // callbacks: {
+  //   async signIn({ user, account, profile }: any) {
+  //     if (account.provider === "github") {
+  //       // connectToDb();
+  //       try {
+  //         const user = await User.findOne({ where: { email: profile.email } });
+
+  //         if (!user) {
+  //           await User.create({
+  //             username: profile.login,
+  //             email: profile.email,
+  //             image: profile.avatar_url,
+  //           });
+  //         }
+  //       } catch (err) {
+  //         console.log(err);
+  //         return false;
+  //       }
+  //     }
+  //     return true;
+  //   },
+  //   ...authConfig.callbacks,
+  // },
   callbacks: {
+    // async session({ session, token }) {
+    //   if (session && token.sub) {
+    //     session.user.id = token.sub
+    //   }
+    //   if (session.user && token.role) {
+    //     session.user.role = token.role as string
+    //   }
+    //   if (session.user) {
+    //     session.user.isTwoFactorEnabled = token.isTwoFactorEnabled as boolean
+    //     session.user.name = token.name
+    //     session.user.email = token.email as string
+    //     session.user.isOAuth = token.isOAuth as boolean
+    //     session.user.image = token.image as string
+    //   }
+    //   return session
+    // },
+    // async jwt({ token }) {
+    //   if (!token.sub) return token
+    //   const existingUser = await db.query.users.findFirst({
+    //     where: eq(users.id, token.sub),
+    //   })
+    //   if (!existingUser) return token
+    //   const existingAccount = await db.query.accounts.findFirst({
+    //     where: eq(accounts.userId, existingUser.id),
+    //   })
+
+    //   token.isOAuth = !!existingAccount
+    //   token.name = existingUser.name
+    //   token.email = existingUser.email
+    //   token.role = existingUser.role
+    //   token.isTwoFactorEnabled = existingUser.twoFactorEnabled
+    //   token.image = existingUser.image
+    //   return token
+    // },
     async signIn({ user, account, profile }: any) {
       if (account.provider === "github") {
         // connectToDb();
@@ -76,6 +136,5 @@ export const {
       }
       return true;
     },
-    ...authConfig.callbacks,
   },
 });
