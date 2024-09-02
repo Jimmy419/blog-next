026c147acfd8ad1e60973e665eae5666ff8ffece
Jimmy419 <suifeng419@126.com>
2024-09-02 15:15:50 +0800

    refine log show ui

--- src/components/blogShow/BlogShow.tsx
+++ src/components/blogShow/BlogShow.tsx
@@ -1,39 +1,21 @@
 "use client";
-// import { MdPreview } from "md-editor-rt";
-import { MdCatalog, MdEditor } from "md-editor-rt";
-import { useEffect, useState } from "react";
+import { MdCatalog, MdPreview } from "md-editor-rt";
 import "md-editor-rt/lib/preview.css";
+import { useState } from "react";
 
 const BlogShow = ({ content }: { content: string }) => {
-  // const [id] = useState("preview-only");
-  // const [scrollElement, setScrollElement] = useState("");
-  // const [pageLoaded, setPageLoaded] = useState(false);
+  const [id] = useState("preview-only");
+  const [scrollElement] = useState("body");
 
-  // useEffect(() => {
-  //   setPageLoaded(true);
-  //   setScrollElement("body");
-  // }, []);
-
-  // if (!pageLoaded) return <></>;
-  // const scrollElement = document.documentElement;
-  // return <MdPreview editorId="mdEditorTwo" theme="dark" modelValue={content} />;
-  // return (
-  //   <>
-  //     <MdEditor editorId={id} modelValue={content} toolbars={[]}/>
-  //     <MdCatalog editorId={id} scrollElement={scrollElement} />
-  //   </>
-  // );
-
-  // return (
-  //   <MdEditor
-  //   editorId="mdEditor3"
-  // pageFullscreen
-  //   theme="dark"
-  //   modelValue={content}
-  //   toolbars={[]}
-  // />
-  // )
-
-  return <div dangerouslySetInnerHTML={{ __html: content }}></div>
+  return (
+    <div className="flex w-full flex-col md:flex-row">
+      <MdCatalog
+        className="md:flex-none md:w-1/5"
+        editorId={id}
+        scrollElement={scrollElement}
+      />
+      <MdPreview className="md:flex-auto" editorId={id} modelValue={content} />
+    </div>
+  );
 };
 export default BlogShow;
