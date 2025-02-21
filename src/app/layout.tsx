39022fc450b1c446407196649dcb4bbdf340697a
Jimmy419 <suifeng419@126.com>
2025-02-21 18:56:58 +0800

    try to add aichat here

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,7 +26,7 @@ export default function RootLayout({
           <ThemeProvider attribute="class">
             <Navbar />
             {children}
-            <Ai />
+            {/* <Ai /> */}
             <Footer />
           </ThemeProvider>
         </Providers>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import { getBlogList } from "@/actions/blog.action";
+import { ChatWindow } from "@/components/aiChat/ChatWindowTwo";
 import { auth } from "@/lib/auth";
 import { Camera } from "lucide-react";
 import Link from "next/link";
@@ -9,7 +10,14 @@ export default async function Home() {
 
   return (
     <main className="flex min-h-[calc(100vh-100px)] flex-col items-center justify-between p-24">
-      <Camera color="red" size={48} />;
+      {/* <Camera color="red" size={48} />; */}
+      <ChatWindow
+        apiBaseUrl={
+          process.env.NEXT_PUBLIC_API_BASE_URL ?? "http://127.0.0.1:8080"
+        }
+        titleText="VO2 Asia 🦜🔗"
+        placeholder="Ask anything..."
+      ></ChatWindow>
       <div>
         {list.map((item) => (
           <div key={item.id}>
